c271228a4411ddc57be449543419776d99687356
ahj30 <ahj30@georgetown.edu>
2020-02-21 19:28:11 -0500

    Final code

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,7 +7,7 @@ import csv
 import os
 import datetime
 from dotenv import load_dotenv
-#from matplotlib import pyplot as plt
+
 
 
 load_dotenv()
@@ -35,17 +35,17 @@ request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&sym
 weekly_request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey={API_KEY}"  
 response = requests.get(request_url)
 weekly_response = requests.get(weekly_request_url)
-#print(type(response))
-#print(response.status_code)
-#print(response.text)
-#print(request_url)
 
 # handle response erors:
 
-if "Error Message" in response.text:
+if "Error Message" in weekly_response.text:
     print("SORRY, SYMBOL NOT FOUND. PLEASE RUN THE PROGRAM AGAIN WITH A VALID SYMBOL.")
     exit()
+if  "Thank you for using Alpha Vantage!" in weekly_response.text:
+    print("TOO MANY REQUESTS. PLEASE WAIT 30 SECONDS AND TRY AGAIN.")
+    exit()
 
+#parse the stock data
 
 parsed_response = json.loads(response.text)
 weekly_parsed_response = json.loads(weekly_response.text)
@@ -65,7 +65,7 @@ low_prices = []
 year_highprices = []
 year_lowprices = []
 
-
+#calculate high and low prices
 
 for wdate in weekly_dates[0:51]:
     year_high = tsdw[wdate]["2. high"]
@@ -87,12 +87,12 @@ recent_low = min(low_prices)
 
 
 
-#latest_close = parsed_response["Time Series (Daily)"]["2020-02-18"]["4. close"]
 
 
 #
 #INFO OUTPUTS
 #
+#writing to CSV
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 
@@ -110,6 +110,9 @@ with open(csv_file_path, "w", newline='') as csv_file: # "w" means "open the fil
             "volume": daily_prices["5. volume"]
         })
 
+
+#output
+
 print("-------------------------")
 print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
@@ -118,27 +121,27 @@ print("REQUEST AT: " + now.strftime("%Y-%m-%d %I:%M %p"))
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH (100 TRADING DAYS): {to_usd(float(recent_high))}")
-print(f"RECENT LOW (100 TRADING DAYS): {to_usd(float(recent_low))}")
+#print(f"RECENT HIGH (100 TRADING DAYS): {to_usd(float(recent_high))}")     #if want to display recent high and recent low
+#print(f"RECENT LOW (100 TRADING DAYS): {to_usd(float(recent_low))}")
 print(f"52 WEEK HIGH: {to_usd(float(fiftytwo_high))}")
 print(f"52 WEEK LOW: {to_usd(float(fiftytwo_low))}")
 print("-------------------------")
 
 
 ###recommendations:
-#### if the stocks latest close is within 5% of recent high, sell. If stock is within 5% of recent low, buy.
-#### otherwise hold or take no position
+#### if the stocks latest close is within 5% of 52wk high, sell. If stock is within 5% of 52 wk low, buy.
+#### otherwise hold or take no position 
 
 ### CHECK IF STOCK IS IN A RANGE
-if float(latest_close) > .95*float(recent_high): 
+if float(latest_close) > .95*float(fiftytwo_high): 
     print("RECOMMENDATION: SELL")
-    print("RECOMMENDATION REASON: STOCK IS WITHIN 5% OF ITS MOST RECENT HIGH")
-elif float(latest_close) < 1.05*float(recent_low):
+    print("RECOMMENDATION REASON: STOCK IS WITHIN 5% OF ITS 52 WEEK HIGH")
+elif float(latest_close) < 1.05*float(fiftytwo_low):
     print("RECOMMENDATION: BUY")
-    print("RECOMMENDATION REASON: STOCK IS WITHIN 5% OF ITS MOST RECENT LOW")
+    print("RECOMMENDATION REASON: STOCK IS WITHIN 5% OF ITS 52 WEEK LOW")
 else:
     print("RECOMMENDATION: HOLD")
-    print("RECCOMENDATION REASON: STOCK IS TRADING WITHIN A CONSOLIDATION RANGE")
+    print("RECCOMENDATION REASON: STOCK IS TRADING WITHIN A CONSOLIDATION RANGE. WAIT TO TAKE ACTION.")
     
 
 print("-------------------------")
@@ -148,24 +151,4 @@ print("HAPPY INVESTING!")
 print("-------------------------")
 
 
-#print("WOULD YOU LIKE TO SEE THE STOCK GRAPH FOR THE PAST 100 TRADING DAYS?")
-
-#while True:
-#    chart_choice = input(str("PLEASE ENTER 'YES' OR 'NO': "))
-#    chart_choice = chart_choice.upper()
-#
-#
-#    if chart_choice == "YES":
-#        print("GENERATING CHART")
-#        break
-#    elif chart_choice == "NO":
-#        print("HAPPY INVESTING!")
-#        print("-------------------------")
-#        break
-#    else:
-#        print("INVALID ENTRY")
-#
-#print("END")
-#
-#
 
